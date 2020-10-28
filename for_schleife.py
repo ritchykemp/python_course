9908a3c782a2671a2c7c71e6b5ddd14d09e56f1c
Richard Kempert <ritchyk@Ri-NineteenEightyFour.local>
2020-10-28 13:11:39 +0100

    kennen lernen der For- Schleife

---
+++ for_schleife.py
@@ -0,0 +1,17 @@
+#for - schleife Beispiele
+
+#test range funktion
+"""for i in range (5): # in nimmt alle werte der Zahlenkette an
+    print(i) #range startet bei 0 und es werden in diesem fall 5 werte erzeugt (ohne 5 ) """
+for i in range (4 ,20 ,2):
+    print (i)
+
+for i in "Hallo Welt": #jeder einzelne buchstabe wird abgearbeitet
+    print (i)
+
+
+
+
+
+
+print ("done.")
