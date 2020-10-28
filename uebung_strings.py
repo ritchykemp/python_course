9649d94e5536712f593ce024c901ee2f1361ebde
Richard Kempert <ritchyk@Ri-NineteenEightyFour.local>
2020-10-28 16:54:39 +0100

    vorbereitung übungsaufgabe string

---
+++ uebung_strings.py
@@ -0,0 +1,20 @@
+""" Teil 1
+Überprüfung von Benutzereingabe ob es sich um einen Strick handelt
+dafür soll die try / except & die Funktion int() verwednet werden """
+
+
+
+
+""" Teil 2
+umwandelung der Strgin benutzereingabe und ausgabe 
+- Großbuchstaben in Kleinbuchstaben 
+- Kleinbuchstaben in Großbuchstaben
+
+Dafür benutzen einer FOR schleife & folgende String - Funktionen
+- str.islower()
+- str.isupper()
+- str.upper()
+- str.lower()
+"""
+
+print ("done.")
