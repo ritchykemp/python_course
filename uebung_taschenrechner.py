2af7cd51dc6dfff2bd168449694afa7a532055ba
Richard Kempert <ritchyk@rininetghtyfour.home>
2020-11-09 16:43:34 +0100

    einfaches Taschenrechner programm installieren

--- uebung_taschenrechner.py
+++ uebung_taschenrechner.py
@@ -8,23 +8,29 @@ mi = "-"
 mu = "*"
 ge = "/"
 ergebnis = 0
-zahl1 = int(input("Geben sie die erste Zahl ein: "))
-zahl2 = int(input("Geben sie die zweite Zahl ein:"))
 
-operator = input("Geben sie einen Mathematischen Operator ein. Die Möglichkeiten sind: + - * /: ")
+while True:
+    zahl1 = int(input("Geben sie die erste Zahl ein: "))
+    zahl2 = int(input("Geben sie die zweite Zahl ein:"))
 
+    operator = input("Geben sie einen Mathematischen Operator ein. Die Möglichkeiten sind: + - * /: ")
 
-if operator == pl:
-    print("debug", operator)
-    ergebnis = zahl1 + zahl2
-elif: operator == mi:
-    ergebnis = zahl1 - zahl2
-elif: operator == mu:
-    ergebnis = zahl1 * zahl2
-elif: operator == ge:
-    ergebnis = zahl1/zahl2
-else:
-    print("Das war kein Operator nochmal")
+    if operator == pl:
+        print("debug", operator)
+        ergebnis = zahl1 + zahl2
+        break
+    elif operator == mi:
+        ergebnis = zahl1 - zahl2
+        break
+    elif operator == mu:
+        ergebnis = zahl1 * zahl2
+        break
+    elif operator == ge:
+        ergebnis = zahl1/zahl2
+        break
+    else:
+        print("Das war kein Operator nochmal")
+        continue
 
 print("Das Ergebniss von: ", zahl1, operator, zahl2, "=", ergebnis)
 print("done.")
